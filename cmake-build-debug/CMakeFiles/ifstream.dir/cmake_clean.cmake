8473f99935c9f09f5568338d8bda49963a453d69
Cochr <cochrantristan@gmail.com>
2020-04-16 11:35:34 -0400

    Added ifstream review

---
+++ cmake-build-debug/CMakeFiles/ifstream.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ifstream.dir/ifstream.cpp.obj"
+  "ifstream.exe"
+  "ifstream.exe.manifest"
+  "ifstream.pdb"
+  "libifstream.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ifstream.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
